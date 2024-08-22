f61163dadde0a490f4ef9c3e05f42ca8b1b1665d
sezerz1121 <85015473+sezerz1121@users.noreply.github.com>
2024-08-23 01:04:39 +0530

    Update cloudinary.js

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -4,7 +4,7 @@ import  dotenv  from "dotenv";
 dotenv.config();
 
 cloudinary.config({ 
-  cloud_name:`${dcn7h7o7iprocess.env.cloud_name}` , 
+  cloud_name:`${process.env.cloud_name}` , 
   api_key: `${process.env.api_key}`, 
   api_secret: `${process.env.api_secret}` 
 });
