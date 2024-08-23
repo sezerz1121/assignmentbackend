cc968c9136c2c310825e125cdbabbaeeaabb8949
sezerz1121 <85015473+sezerz1121@users.noreply.github.com>
2024-08-23 23:49:13 +0530

    Update scan.routes.js

--- src/routes/scan.routes.js
+++ src/routes/scan.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { Scan } from "../controllers/scan.controllers.js";
+import { Scan, Data } from "../controllers/scan.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
@@ -8,8 +8,6 @@ router.route("/scan").post(
     upload.fields([{ name: "fileScan", maxCount: 1 }]), 
     Scan
 );
-
 router.route("/data").get(Data);
 
 export default router;
-
